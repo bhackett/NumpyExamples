94b6ca5500a288ec7c0b65f2e7a579da9bd0e163
bhackett <bkhackett@gmail.com>
2021-01-15 17:42:37 -0500

    Numpy and Python array comparison

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
 import numpy as np
 
+# nlist = [1, 2, 3, 4, 5, 6]
+# nparray = np.array(nlist)
+# print(nlist)
+# print(nparray)
 
+L = [1, 2, 3]
+NA = np.array(L)
+for i in L:
+    print(i)
+for i in NA:
+    print(i)
+
+L1 = L + [4]
+print(L1)
+L1.append(5)
+print(L1)
+
+NA1 = NA + [4]
+print(NA1)
+
+
+
+
+print("Finished correctly")
