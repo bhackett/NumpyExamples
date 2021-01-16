25646f89e9e867b88d2770089a3c8e2f58307887
bhackett <bkhackett@gmail.com>
2021-01-15 21:56:12 -0500

    Added sum(), sum(axis=0)), np.zeros(), np.ones() np.ones( (x, y), dtype=np.int16) np.empty((x, y), dtype=np.float) np.arange(x, y) np.linspace(x, y, num=10) np.random.random((x, y)) ary.reshape()

--- main.py
+++ main.py
@@ -35,7 +35,9 @@ import numpy as np
 # print(np.exp(NA))
 
 a = np.array([1, 2, 3])
-b = np.array([[1, 2], [3, 4], [5, 6]])
+b = np.array([[1, 2],
+              [3, 4],
+              [5, 6]])
 print(a[0])  # 1
 print(b[0])  # [1 2]
 print(b[0][0])  # 1
@@ -54,13 +56,46 @@ d = np.array([1.1, 1.2, 1.3])
 print(d.dtype)  # float64
 e = np.array([1, 2], dtype=np.float64)
 print(e)        # [1. 2.]
-print(b.itemsize)  # 4
+print(b.itemsize)  # 4  number of bytes
 print(a.itemsize)  # 4
 print(e.itemsize)  # 8
-print(a.min())     # 1
+print(a.min())     # 1  lowest value
 print(b.min())     # 1
 print(e.min())     # 1.0
-print(e.max())     # 2.0
+print(e.max())     # 2.0 highest value
+print(b.sum())     # 21  sum all elements
+print(e.sum())     # 3.0
+print(b.sum(axis=0))  # [9 12]  sum of columns
+print(b.sum(axis=1))  # [3 7 11]  sum of rows
+
+f = np.zeros((2, 3))  # initialize array
+print(f)          # [[0. 0. 0.] [0. 0. 0.]]
+print(f.itemsize)     # 8
+f = np.ones((2, 3))
+print(f)          # [[1. 1. 1.] [1. 1. 1.]]
+f = np.ones((3, 2), dtype=np.int16)
+print(f)          # [[1 1] [1 1] [1 1]]
+f = np.empty((2, 2), dtype=np.float)
+print(f)   # [[6.11598403e-312 6.11598400e-312] [6.11598403e-312 6.11598403e-312]]
+g = np.arange(1, 5)
+print(g)   # [1 2 3 4]
+g = np.arange(1, 5, .5)
+print(g)   # [1.  1.5 2.  2.5 3.  3.5 4.  4.5]
+h = np.linspace(1, 5, num=10)
+print(h)   # linear array from 1 to 5 evenly spaced [1. ... 8 other numbers ... 5]
+i = np.random.random((2, 3))
+print(i)   # [[0.83304826 0.57155644 0.25289799] [0.74402872 0.69212865 0.43486987]]
+j = np.zeros((2, 3))
+print(j)   # [[0. 0. 0.] [0. 0. 0.]]
+print(j.reshape((3, 2)))  # [[0. 0.] [0. 0.] [0. 0.]]
+k = np.ones((1, 9))
+print(k)   # [[1. 1. 1. 1. 1. 1. 1. 1. 1.]]
+l = k.reshape((3, -1))
+print(l)   # [[1. 1. 1.] [1. 1. 1.] [1. 1. 1.]]
+m = np.zeros((3, 1))
+print(m)   # [[0.] [0.] [0.]]
+
+
 
 
 print("Finished correctly")
